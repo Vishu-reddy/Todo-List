02e3ba52aa90bfac930ccd238da0df13e040d9c2
vishu reddy <saduvishwanath95@gmail.com>
2024-03-28 07:49:18 +0530

    add work page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 let newitem=["wakeup at 5","practice web development","get ready for college"];
+let workitem=[];
 app.get("/", (req, res) => {
   let today = new Date();
   let week = today.getDay();
@@ -15,15 +16,30 @@ app.get("/", (req, res) => {
   };
   let day = today.toLocaleDateString("en-us", options);
   res.render("list", {
-    kindofday: day,
-    newitem:newitem
+    Listtitle: day,
+    items:newitem
   });
 });
 app.post("/",function(req,res){
- item=req.body.newitem;
- newitem.push(item);
- res.redirect("/");
+  item=req.body.newitem;
+  if(req.body.list==="Work"){
+    workitem.push(item);
+    res.redirect("/work");
+  }else{
+     newitem.push(item);
+     res.redirect("/");
+  }
+  
+
 });
+app.get("/work",(req,res)=>{
+  res.render("list",{Listtitle:"Work",items:workitem});
+});
+app.post("/work",(req,res)=>{
+  let item=req.body.newitem;
+  workitem.push(item);
+  res.redirect("/work");
+})
 app.listen(3000, () => {
   console.log("sever started on port 3000");
 });
